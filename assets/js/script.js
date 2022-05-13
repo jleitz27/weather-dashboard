7734c77ed1cec50f5ed5d16a2e061e842f3b41df
Jason L <jkleitz27@gmail.com>
2022-05-13 12:17:07 -0400

    variables added to js

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+
+var  cityFormEl = document.querySelector("#city-form");
+var cityInputEl = document.querySelector("#city");
+var weatherEl = document.querySelector("#current-weather");
+var citySearchInputEl = document.querySelector("#city-search");
+var forecastTitle = document.querySelector("#forecast");
+var forecastContainerEl = document.querySelector("#fiveday");
+var pastCityButtonEl = document.querySelector("#past-city-buttons");
