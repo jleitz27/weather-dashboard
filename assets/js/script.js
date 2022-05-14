3f5fcd9612474c57574074fed31ab15b98aadaae
Jason L <jkleitz27@gmail.com>
2022-05-14 09:35:36 -0400

    updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,43 +25,66 @@ var getCityWeather = function(city){
     });
 };
 
-$("#submit").on("click", function() {
+var displayWeather = function(weather, searchCity){
+    //clear old content
+    weatherContainerEl.textContent= "";  
+    citySearchInputEl.textContent=searchCity;
+ 
+    //console.log(weather);
+ 
+    //create date element
+    var currentDate = document.createElement("span")
+    currentDate.textContent=" (" + moment(weather.dt.value).format("MMM D, YYYY") + ") ";
+    citySearchInputEl.appendChild(currentDate);
+ 
+    //create an image element
+    var weatherIcon = document.createElement("img")
+    weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
+    citySearchInputEl.appendChild(weatherIcon);
+ 
+    //create a span element to hold temperature data
+    var temperatureEl = document.createElement("span");
+    temperatureEl.textContent = "Temperature: " + weather.main.temp + " °F";
+    temperatureEl.classList = "list-group-item"
+   
+    //create a span element to hold Humidity data
+    var humidityEl = document.createElement("span");
+    humidityEl.textContent = "Humidity: " + weather.main.humidity + " %";
+    humidityEl.classList = "list-group-item"
+ 
+    //create a span element to hold Wind data
+    var windSpeedEl = document.createElement("span");
+    windSpeedEl.textContent = "Wind Speed: " + weather.wind.speed + " MPH";
+    windSpeedEl.classList = "list-group-item"
+ 
+    //append to container
+    weatherContainerEl.appendChild(temperatureEl);
+ 
+    //append to container
+    weatherContainerEl.appendChild(humidityEl);
+ 
+    //append to container
+    weatherContainerEl.appendChild(windSpeedEl);
+ 
+    var lat = weather.coord.lat;
+    var lon = weather.coord.lon;
+    getUvIndex(lat,lon)
+ }
 
-    $('#forecast').addClass('show');
-
-    // get the value of the input from user
-    city = $(citySearch).val();
-    
-    // clear input box
-    $(citySearch).val("");  
-
-    // full url to call api
-    var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey;
-
-    $.ajax({
-        url: queryUrl,
-        method: "GET"
-    })
-    .then(function (response){
-
-        console.log(response)
-    
-        console.log(response.name)
-        console.log(response.weather[0].icon)
-    
-        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
-        console.log(Math.floor(tempF))
-    
-        console.log(response.main.humidity)
-    
-        console.log(response.wind.speed)
-    
-        getCurrentConditions(response);
-        // getCurrentForecast(response);
-        // makeList();
-    
-        })
-    });
+var formSumbitHandler = function(event){
+    event.preventDefault();
+    var city = cityInputEl.value.trim();
+    if(city){
+        getCityWeather(city);
+        get5Day(city);
+        cities.unshift({city});
+        cityInputEl.value = "";
+    } else{
+        alert("Please enter a City");
+    }
+    saveSearch();
+    pastSearch(city);
+}
 
     // function for current conditions
     function getCurrentConditions (response) {
